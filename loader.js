f790123d2662a5d88b8ab26233da62d715b26967
Siphe23 <siphesihlemzamane@gmai.com>
2026-03-09 20:01:22 +0200

    updated files

---
+++ loader.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    const loader = document.getElementById('loader');
+    if(loader) loader.classList.add('hide');
+  }, 1500);
+});
